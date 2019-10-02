4629b3fcc0d9a05ae6a5baf888c266a17a26f954
Lmar Niazman <lmar.niazman@capgemini.com>
2019-10-02 15:33:46 +0200

    Fix for #31: Moved test files and refactored imports in the registrar

---
+++ src/test/files/scope/DevonArchitectureScopeApi2ImplCheck.java
@@ -0,0 +1,23 @@
+/*
+ * This is a header...
+ */
+package com.devonfw.ide.sonarqube.common.api;
+
+import com.devonfw.ide.sonarqube.common.impl.Bar; // Noncompliant
+
+class MyClass {
+
+  private com.devonfw.ide.sonarqube.common.impl.Foo foo; // Noncompliant
+
+  private Bar bar;
+
+  public com.devonfw.ide.sonarqube.common.impl.Foo getFoo() { // Noncompliant
+    return this.foo;
+  }
+
+  public void init() {
+
+    this.foo = new com.devonfw.ide.sonarqube.common.impl.Foo();// Noncompliant
+  }
+
+}
