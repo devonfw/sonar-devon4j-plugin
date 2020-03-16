f231b734f8f04e6b9e8147d93e28d835628e58fe
Lmar Niazman <lmar.niazman@capgemini.com>
2020-03-16 15:08:05 +0100

    Fix for #73

--- src/test/java/com/devonfw/ide/sonarqube/common/impl/check/component/DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheckTest.java
+++ src/test/java/com/devonfw/ide/sonarqube/common/impl/check/component/DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheckTest.java
@@ -5,30 +5,34 @@ import org.sonar.java.checks.verifier.JavaCheckVerifier;
 
 /**
  * Test of {@link DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck}.
+ *
+ * TODO: After PR #75 has been merged, merge master into this branch. Tests should then all be fine and PR can be
+ * created.
+ *
  */
 public class DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheckTest {
 
-  // /**
-  // * Test of {@link DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck}.
-  // */
-  // @Test
-  // public void testOKSameComponent() {
-  //
-  // JavaCheckVerifier.verifyNoIssue(
-  // "src/test/files/component/DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck_OKSameComponent.java",
-  // new DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck());
-  // }
+  /**
+   * Test of {@link DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck}.
+   */
+  @Test
+  public void testOKSameComponent() {
 
-  // /**
-  // * Test of {@link DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck}.
-  // */
-  // @Test
-  // public void testOKDifferentComponent() {
-  //
-  // JavaCheckVerifier.verifyNoIssue(
-  // "src/test/files/component/DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck_OKDifferentComponent.java",
-  // new DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck());
-  // }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/component/DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck_OKSameComponent.java",
+        new DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck());
+  }
+
+  /**
+   * Test of {@link DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck}.
+   */
+  @Test
+  public void testOKDifferentComponent() {
+
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/component/DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck_OKDifferentComponent.java",
+        new DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck());
+  }
 
   /**
    * Test of {@link DevonArchitectureLayerDataaccess2Dataaccess4ComponentCheck}.
