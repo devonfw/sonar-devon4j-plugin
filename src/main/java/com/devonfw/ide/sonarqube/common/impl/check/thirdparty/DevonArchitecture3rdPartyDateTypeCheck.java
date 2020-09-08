cba5a4ff70e0bb83fc2483b86fdda8d9f6d0d6fe
ADPRUS <adrianna.prus@capgemini.com>
2020-09-08 12:11:31 +0200

    implemented new rule checking date types and added tests, html with description

---
+++ src/main/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyDateTypeCheck.java
@@ -0,0 +1,32 @@
+package com.devonfw.ide.sonarqube.common.impl.check.thirdparty;
+
+import com.devonfw.ide.sonarqube.common.api.JavaType;
+import com.devonfw.ide.sonarqube.common.impl.check.DevonArchitecture3rdPartyCheck;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+
+/**
+ * {@link DevonArchitecture3rdPartyCheck} verifying that proper date types are used.
+ */
+@Rule(key = "E7", name = "devonfw 3rd Party Date Type Check", //
+    priority = Priority.BLOCKER, tags = { "architecture-violation", "devonfw", "thirdparty" })
+public class DevonArchitecture3rdPartyDateTypeCheck extends DevonArchitecture3rdPartyCheck {
+
+    @Override protected String checkDependency(JavaType source, JavaType target) {
+
+        String targetString = target.toString();
+        if(targetString.equals("java.util.Date")){
+            return "Use the java.time.LocalDate[Time] instead";
+        }
+        if(targetString.equals("java.util.Calendar")){
+            return "Use java.time.LocalDate[Time], java.time.ZonedDateTime or java.time.OffsetDateTime instead";
+        }
+        if(targetString.equals("java.sql.Date")){
+            return "Use java.time.LocalDate instead";
+        }
+        if (targetString.equals("java.sql.Timestamp")){
+            return "Use java.time.Instant instead";
+        }
+        return null;
+    }
+}
