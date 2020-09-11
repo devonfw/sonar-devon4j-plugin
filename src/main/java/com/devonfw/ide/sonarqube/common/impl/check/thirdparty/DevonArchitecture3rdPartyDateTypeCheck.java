88283079535674b38a4dc3a626b00376ac18d31e
adprus <adrianna.prus@capgemini.com>
2020-09-11 09:03:24 +0200

    #94: reformatted code

--- src/main/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyDateTypeCheck.java
+++ src/main/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyDateTypeCheck.java
@@ -1,9 +1,10 @@
 package com.devonfw.ide.sonarqube.common.impl.check.thirdparty;
 
-import com.devonfw.ide.sonarqube.common.api.JavaType;
-import com.devonfw.ide.sonarqube.common.impl.check.DevonArchitecture3rdPartyCheck;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
+
+import com.devonfw.ide.sonarqube.common.api.JavaType;
+import com.devonfw.ide.sonarqube.common.impl.check.DevonArchitecture3rdPartyCheck;
 
 /**
  * {@link DevonArchitecture3rdPartyCheck} verifying that proper date types are used.
@@ -12,22 +13,23 @@ import org.sonar.check.Rule;
     priority = Priority.MINOR, tags = { "architecture-violation", "devonfw", "thirdparty" })
 public class DevonArchitecture3rdPartyDateTypeCheck extends DevonArchitecture3rdPartyCheck {
 
-    @Override protected String checkDependency(JavaType source, JavaType target) {
+  @Override
+  protected String checkDependency(JavaType source, JavaType target) {
 
-        String targetString = target.toString();
+    String targetString = target.toString();
 
-        if (targetString.equals("java.util.Date")) {
-            return "Use the java.time.LocalDate[Time] instead";
-        }
-        if (targetString.equals("java.util.Calendar")) {
-            return "Use java.time.LocalDate[Time], java.time.ZonedDateTime or java.time.OffsetDateTime instead";
-        }
-        if (targetString.equals("java.sql.Date")) {
-            return "Use java.time.LocalDate instead";
-        }
-        if (targetString.equals("java.sql.Timestamp")) {
-            return "Use java.time.Instant instead";
-        }
-        return null;
+    if (targetString.equals("java.util.Date")) {
+      return "Use the java.time.LocalDate[Time] instead";
     }
+    if (targetString.equals("java.util.Calendar")) {
+      return "Use java.time.LocalDate[Time], java.time.ZonedDateTime or java.time.OffsetDateTime instead";
+    }
+    if (targetString.equals("java.sql.Date")) {
+      return "Use java.time.LocalDate instead";
+    }
+    if (targetString.equals("java.sql.Timestamp")) {
+      return "Use java.time.Instant instead";
+    }
+    return null;
+  }
 }
