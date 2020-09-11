060f07fc4e53279855b193d17442f4f389ca141d
Robert <robert.sokolski@capgemini.com>
2020-09-11 10:19:44 +0200

    #95: code reformatted, removed Set with imports, removed unnecessary condition

--- src/main/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyImmutableCheck.java
+++ src/main/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyImmutableCheck.java
@@ -1,31 +1,27 @@
 package com.devonfw.ide.sonarqube.common.impl.check.thirdparty;
 
-import com.devonfw.ide.sonarqube.common.api.JavaType;
-import com.devonfw.ide.sonarqube.common.impl.check.DevonArchitecture3rdPartyCheck;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.devonfw.ide.sonarqube.common.api.JavaType;
+import com.devonfw.ide.sonarqube.common.impl.check.DevonArchitecture3rdPartyCheck;
 
 /**
- * {@link DevonArchitecture3rdPartyCheck} verifying that Immutable is used from the proper (org.hibernate.annotations) package.
+ * {@link DevonArchitecture3rdPartyCheck} verifying that Immutable is used from the proper (org.hibernate.annotations)
+ * package.
  */
 @Rule(key = "E8", name = "devonfw 3rd Party Immutable Check", //
     priority = Priority.CRITICAL, tags = { "architecture-violation", "devonfw", "thirdparty" })
 public class DevonArchitecture3rdPartyImmutableCheck extends DevonArchitecture3rdPartyCheck {
 
-    private Set<String> allImports = new HashSet<>();
+  @Override
+  protected String checkDependency(JavaType source, JavaType target) {
 
-    @Override protected String checkDependency(JavaType source, JavaType target) {
+    if (source.getSimpleName().contains("Entity")
+        && target.toString().equals("javax.annotation.concurrent.Immutable")) {
 
-        allImports.add(target.toString());
-        String sourceSimpleName = source.getSimpleName();
-        if (sourceSimpleName.contains("Entity") && allImports.contains("javax.annotation.concurrent.Immutable")
-            && allImports.contains("javax.persistence.Entity")) {
-
-            return "Use Immutable from org.hibernate.annotations.Immutable in Entity class.";
-        }
-        return null;
+      return "Use Immutable from org.hibernate.annotations.Immutable in Entity class.";
     }
+    return null;
+  }
 }
--- src/test/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyImmutableCheckTest.java
+++ src/test/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyImmutableCheckTest.java
@@ -8,33 +8,38 @@ import org.sonar.java.checks.verifier.JavaCheckVerifier;
  */
 public class DevonArchitecture3rdPartyImmutableCheckTest {
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyImmutableCheck} verifies that the use of Immutable from javax package in Entity class is not allowed.
-     */
-    @Test
-    public void testNotOKJavaxImmutableEntityClass() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyImmutableCheck} verifies that the use of Immutable from javax package in
+   * Entity class is not allowed.
+   */
+  @Test
+  public void testNotOKJavaxImmutableEntityClass() {
 
-        JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyImmutableCheck_JavaxImmutableEntityClass.java",
-            new DevonArchitecture3rdPartyImmutableCheck());
-    }
+    JavaCheckVerifier.verify(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyImmutableCheck_JavaxImmutableEntityClass.java",
+        new DevonArchitecture3rdPartyImmutableCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyImmutableCheck} verifies that the use of Immutable from javax package in other class is allowed.
-     */
-    @Test
-    public void testOKJavaxImmutableNonEntityClass() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyImmutableCheck} verifies that the use of Immutable from javax package in
+   * other class is allowed.
+   */
+  @Test
+  public void testOKJavaxImmutableNonEntityClass() {
 
-        JavaCheckVerifier.verifyNoIssue("src/test/files/thirdparty/DevonArchitecture3rdPartyImmutableCheck_JavaxImmutableNonEntityClass.java",
-            new DevonArchitecture3rdPartyImmutableCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyImmutableCheck_JavaxImmutableNonEntityClass.java",
+        new DevonArchitecture3rdPartyImmutableCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyImmutableCheck} verifies that Immutable is used from proper package in Entity class.
-     */
-    @Test
-    public void testOK() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyImmutableCheck} verifies that Immutable is used from proper package in
+   * Entity class.
+   */
+  @Test
+  public void testOK() {
 
-        JavaCheckVerifier.verifyNoIssue("src/test/files/thirdparty/DevonArchitecture3rdPartyImmutableCheck_OK.java",
-            new DevonArchitecture3rdPartyImmutableCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue("src/test/files/thirdparty/DevonArchitecture3rdPartyImmutableCheck_OK.java",
+        new DevonArchitecture3rdPartyImmutableCheck());
+  }
 }
