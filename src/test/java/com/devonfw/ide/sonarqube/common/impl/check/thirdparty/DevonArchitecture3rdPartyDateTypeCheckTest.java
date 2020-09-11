707b52e0234478de0f243c6d7a10a07b5ad5de31
adprus <adrianna.prus@capgemini.com>
2020-09-11 09:55:28 +0200

    #94: refactor tests

--- src/test/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyDateTypeCheckTest.java
+++ src/test/java/com/devonfw/ide/sonarqube/common/impl/check/thirdparty/DevonArchitecture3rdPartyDateTypeCheckTest.java
@@ -8,100 +8,107 @@ import org.sonar.java.checks.verifier.JavaCheckVerifier;
  */
 public class DevonArchitecture3rdPartyDateTypeCheckTest {
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations 'java.util.Date'
-     * is not allowed.
-     */
-    @Test public void testJavaUtilDate() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations
+   * 'java.util.Date' is not allowed.
+   */
+  @Test
+  public void testJavaUtilDate() {
 
-        JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaUtilDate.java",
-            new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaUtilDate.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations 'java.time.LocalDate'
-     * is not flagged.
-     */
-    @Test public void testJavaTimeLocalDate() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations
+   * 'java.time.LocalDate' is not flagged.
+   */
+  @Test
+  public void testJavaTimeLocalDate() {
 
-        JavaCheckVerifier
-            .verifyNoIssue("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeLocalDate.java",
-                new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeLocalDate.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations 'java.time.LocalDateTime'
-     * is not flagged.
-     */
-    @Test public void testJavaTimeLocalDateTime() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations
+   * 'java.time.LocalDateTime' is not flagged.
+   */
+  @Test
+  public void testJavaTimeLocalDateTime() {
 
-        JavaCheckVerifier.verifyNoIssue(
-            "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeLocalDateTime.java",
-            new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeLocalDateTime.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations 'java.util.Calendar'
-     * is not allowed.
-     */
-    @Test public void testJavaUtilCalendar() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations
+   * 'java.util.Calendar' is not allowed.
+   */
+  @Test
+  public void testJavaUtilCalendar() {
 
-        JavaCheckVerifier
-            .verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaUtilCalendar.java",
-                new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaUtilCalendar.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations 'java.time.ZonedDateTime'
-     * is not flagged.
-     */
-    @Test public void testJavaTimeZonedDateTime() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations
+   * 'java.time.ZonedDateTime' is not flagged.
+   */
+  @Test
+  public void testJavaTimeZonedDateTime() {
 
-        JavaCheckVerifier.verifyNoIssue(
-            "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeZonedDateTime.java",
-            new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeZonedDateTime.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations 'java.time.OffsetDateTime'
-     * is not flagged.
-     */
-    @Test public void testJavaTimeOffsetDateTime() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations
+   * 'java.time.OffsetDateTime' is not flagged.
+   */
+  @Test
+  public void testJavaTimeOffsetDateTime() {
 
-        JavaCheckVerifier.verifyNoIssue(
-            "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeOffsetDateTime.java",
-            new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeOffsetDateTime.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations 'java.sql.Date'
-     * is not allowed.
-     */
-    @Test public void testJavaSqlDate() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations
+   * 'java.sql.Date' is not allowed.
+   */
+  @Test
+  public void testJavaSqlDate() {
 
-        JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaSqlDate.java",
-            new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaSqlDate.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations 'java.sql.Timestamp'
-     * is not allowed.
-     */
-    @Test public void testJavaSqlTimestamp() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of discouraged date types annotations
+   * 'java.sql.Timestamp' is not allowed.
+   */
+  @Test
+  public void testJavaSqlTimestamp() {
 
-        JavaCheckVerifier
-            .verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaSqlTimestamp.java",
-                new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verify("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaSqlTimestamp.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 
-    /**
-     * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations 'java.time.Instant'
-     * is not flagged.
-     */
-    @Test public void testJavaTimeInstant() {
+  /**
+   * Test of {@link DevonArchitecture3rdPartyDateTypeCheck} verifies that the use of correct date types annotations
+   * 'java.time.Instant' is not flagged.
+   */
+  @Test
+  public void testJavaTimeInstant() {
 
-        JavaCheckVerifier
-            .verifyNoIssue("src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeInstant.java",
-                new DevonArchitecture3rdPartyDateTypeCheck());
-    }
+    JavaCheckVerifier.verifyNoIssue(
+        "src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeInstant.java",
+        new DevonArchitecture3rdPartyDateTypeCheck());
+  }
 }
