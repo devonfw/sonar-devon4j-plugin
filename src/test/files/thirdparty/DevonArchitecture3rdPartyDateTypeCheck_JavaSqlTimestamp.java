fadfa3e23568243f398682f13876bc9b85779127
Adrianna Prus <70693041+adprus@users.noreply.github.com>
2020-09-21 09:51:56 +0200

    #94: 3rd party rule for date types (#99)
    * implemented new rule checking date types and added tests, html with description
    * add DevonArchitecture3rdPartyDateTypeCheck to DevonSonarRegister
    * correct imports in DevonSonarRegistrar
    * #94: add E7 rule to README.adoc
    * #94: formatted code
    * #94: change priority of rule E7 to MINOR, add this rule to devon4j.xml file
    * #94: reformatted code
    * #94: refactor tests

---
+++ src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaSqlTimestamp.java
@@ -0,0 +1,6 @@
+package com.devonfw.ide.sonarqube.dataaccess.impl;
+
+import java.sql.Timestamp; // Noncompliant
+
+class MyClass {
+}
---
+++ src/test/files/thirdparty/DevonArchitecture3rdPartyDateTypeCheck_JavaTimeLocalDateTime.java
@@ -0,0 +1,6 @@
+package com.devonfw.ide.sonarqube.dataaccess.impl;
+
+import java.time.LocalDateTime; // compliant
+
+class MyClass {
+}
